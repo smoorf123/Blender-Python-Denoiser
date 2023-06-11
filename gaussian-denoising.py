3c3ff2f72735ba8d2fcdd61d98abbc31f61b07b0
Somil Varshney <somil2004@gmail.com>
2023-06-11 16:16:02 +0530

    gaussian denoising implementation

---
+++ gaussian-denoising.py
@@ -0,0 +1,15 @@
+import cv2  # Import the OpenCV library
+
+# Read the image from appropriate path
+image = cv2.imread('./Blender-Denoiser/noisy-img.png')
+
+# Display the image
+cv2.imshow("Image", image)
+
+# Apply gaussian filtering with a 5x5 kernel
+filtered_image = cv2.GaussianBlur(image, (5, 5), 0)
+
+# Display the filtered image
+cv2.imshow('Filtered Image', filtered_image)
+cv2.waitKey(0)  # Wait for a key press to exit
+cv2.destroyAllWindows()  # Close all windows
